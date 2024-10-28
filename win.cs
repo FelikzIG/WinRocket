0e87845ad5442423d9506cdbcb10585f9e9dfc7c
FelikzIG <119817644+FelikzIG@users.noreply.github.com>
2024-10-28 19:50:09 +0000

    Add files via upload

---
+++ win.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class win
+    {
+
+    }
+
+    public class WinRocket
+    {
+        public void tempClear(string tempDir)
+        {
+            foreach (var name in Directory.GetFiles(tempDir))
+            {
+                try
+                {
+                    File.Delete(name);
+                }
+                catch (Exception)
+                {
+                    // Ignore the failure and continue
+                }
+            }
+        }
+    }
+}
